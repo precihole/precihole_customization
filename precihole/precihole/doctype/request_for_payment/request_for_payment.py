d22567bbe6ab206f68aae8083c45e201abe0e9dc
erpadmin@preciholesports.com <erpadmin@preciholesports.com>
2023-12-02 11:17:45 +0530

    fix: Validation for access amount

--- precihole/precihole/doctype/request_for_payment/request_for_payment.py
+++ precihole/precihole/doctype/request_for_payment/request_for_payment.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2023, Precihole and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class RequestforPayment(Document):
 	def before_insert(self):
 		self.status = 'Draft'
+
+@frappe.whitelist()
+def get_request_for_payment_flag():
+	return frappe.db.get_all('Request for Payment', {'reference_doctype': frappe.form_dict.doctype, 'reference_name': frappe.form_dict.id, 'docstatus': 1}, ['sum(paid_amount) as paid_amount'], pluck='paid_amount')
--- precihole/public/js/purchase_order.js
+++ precihole/public/js/purchase_order.js
@@ -3,18 +3,35 @@ frappe.ui.form.on('Purchase Order',  {
         setTimeout(() => {
             frm.remove_custom_button('Payment Request', 'Create'); //hide
         }, 1000);
-        frm.add_custom_button(__('Request for Payment'), function(){
-            frappe.run_serially([ 
-                () => frappe.new_doc('Request for Payment'), 
-                () => {
-                    cur_frm.set_value('party_type', 'Supplier')
-                    cur_frm.set_value('party', frm.doc.supplier)
-                    cur_frm.set_value('reference_doctype', frm.doc.doctype)
-                    cur_frm.set_value('reference_name', frm.doc.name)
-                }
-            ], 
-                //() => next action you want to perform
-            );
-        }, __("Create"));
+
+        if(frm.doc.rounded_total !== frm.doc.advance_paid){
+            frm.add_custom_button(__('Request for Payment'), function(){
+                frappe.call({
+                    method: "precihole.precihole.doctype.request_for_payment.request_for_payment.get_request_for_payment_flag",
+                    args: {
+                        doctype: frm.doc.doctype,
+                        id: frm.doc.name,
+                    },
+                    callback: function (res) {
+                        if(frm.doc.rounded_total == res.message[0]){
+                            frappe.throw('Payment request has already been generated for the amount specified in the purchase order.')
+                        }
+                        else{
+                            frappe.run_serially([ 
+                                () => frappe.new_doc('Request for Payment'), 
+                                () => {
+                                    cur_frm.set_value('party_type', 'Supplier')
+                                    cur_frm.set_value('party', frm.doc.supplier)
+                                    cur_frm.set_value('reference_doctype', frm.doc.doctype)
+                                    cur_frm.set_value('reference_name', frm.doc.name)
+                                }
+                            ], 
+                                //() => next action you want to perform
+                            );
+                        }
+                    }
+                })
+            }, __("Create"));
+        }
     }
 });
